4747e9f083108e556d52ad0f86b2e0de792bc645
milenatesouro <mtesour99@gmail.com>
2018-07-10 09:15:36 -0300

    hecho

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
@@ -1,4 +1,13 @@
 function mostrar()
 {
+    var precio;
+    var porcentajedescuento;
+    precio = prompt ("precio");
+    porcentajedescuento = prompt ("porcentaje de descuento");
+    precio = parseInt (precio);
+    porcentajedescuento = parseInt (porcentajedescuento);
+    var descuento = (precio * porcentajedescuento)/100;
+    var resultado = (precio - descuento);
+    document.getElementById("elPrecioFinal").value = resultado;
 
 }
