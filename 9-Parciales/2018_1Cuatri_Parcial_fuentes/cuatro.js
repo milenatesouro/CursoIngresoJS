181127f41576cfa5ccb042d9e2e093683f30b3e5
milenatesouro <mtesour99@gmail.com>
2018-07-10 10:08:39 -0300

    no terminado

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
@@ -1,4 +1,22 @@
 function mostrar()
 {
+    var numerouno;
+    var numerodos;
+    numerouno = prompt ("Ingrese primer numero");
+    numerodos = prompt ("Ingrese segundo numero");
+    numerouno = parseInt (numerouno);
+    numerodos = parseInt (numerodos);
+    if (numerouno == numerodos) {
+    var iguales = (numero uno numero dos);
+    }
+    else if (numerouno>numerodos) {
+    var resta = (numero uno - numero dos);
+    }
+    else if (numerouno<numerodos) {
+    var suma = (numerouno + numerodos);
+    }
+    else (suma>10) { 
+    alert (" la suma es " + suma + " y supero el 10. ")
+    }
 
 }
