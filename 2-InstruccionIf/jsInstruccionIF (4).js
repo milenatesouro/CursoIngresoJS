63f0a93ed2f46107befc89f66cd158b51313e199
milenatesouro <mtesour99@gmail.com>
2018-07-10 10:30:28 -0300

    no terminado

--- 2-InstruccionIf/jsInstruccionIF (4).js
+++ 2-InstruccionIf/jsInstruccionIF (4).js
@@ -1,7 +1,10 @@
 function mostrar()
 {
 //tomo la edad  
-
-
-
+    var edad;
+    edad = document.getElementById("edad").value;
+    edad = parseInt (edad);
+    if (edad>21) {
+        alert ("No sos adolescente.");
+    }
 }//FIN DE LA FUNCIÓN
