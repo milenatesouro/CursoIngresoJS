f028d0c77ba6ac976adc2224df301724af6bc737
milenatesouro <mtesour99@gmail.com>
2018-07-10 12:17:53 -0300

    no da

--- 2-InstruccionIf/jsInstruccionIF (8).js
+++ 2-InstruccionIf/jsInstruccionIF (8).js
@@ -1,8 +1,12 @@
 function mostrar()
 {
 //tomo la edad  
-
-	
-
-
+    var edad;
+    var estadocivil;
+    edad = document.getElementById(edad).value;
+    estadocivil = document.getElementById(estadoCivil).value;
+    edad = parseInt (edad);
+    if (edad>17 && estadocivil=="soltero") {
+        alert ("Es soltero y no es menor.");
+    }
 }//FIN DE LA FUNCIÓN
