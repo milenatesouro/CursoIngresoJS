978235d90f9d1ea28bdab734ae73e8c60580a016
milenatesouro <mtesour99@gmail.com>
2018-07-10 09:04:07 -0300

    terminado

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -1,5 +1,13 @@
 
 function mostrar()
 {
+    var ancho;
+    var largo;
+    ancho = prompt ("ancho");
+    largo = prompt ("largo");
+    ancho = parseInt (ancho);
+    largo = parseInt (largo);
+    var perimetro = (ancho + largo)*2;
+    alert (" El perimetro es " + perimetro);
 
 }
