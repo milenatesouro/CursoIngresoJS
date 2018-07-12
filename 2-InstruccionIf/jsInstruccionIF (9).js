bc32e7d088f74bbf88760a4e09ae5fbab33d57fd
milenatesouro <mtesour99@gmail.com>
2018-07-12 08:58:11 -0300

    no terminado

--- 2-InstruccionIf/jsInstruccionIF (9).js
+++ 2-InstruccionIf/jsInstruccionIF (9).js
@@ -2,6 +2,7 @@ function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
 		var random;
-		random = (math.floor (math.random ()*(11-10))+1;
+		random = math.floor (math.random () * (11 - 1) + 1 ;
+		alert (random);
 
 }//FIN DE LA FUNCIÓN
