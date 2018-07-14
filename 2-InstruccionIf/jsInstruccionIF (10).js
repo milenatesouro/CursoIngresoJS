f173d31bee7712a3a6aa9f94931620ee14fdfaed
milenatesouro <mtesour99@gmail.com>
2018-07-14 11:08:52 -0300

    listo

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,6 +1,14 @@
 function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
-	
-
+	var nota;
+	nota = Math.floor((Math.random() * 10) + 1);
+	alert (nota);
+	if (nota>=9) {
+		alert (" Excelente. ");
+	} else if (nota>4) {
+		alert(" Aprobó. ");
+	} else if (nota<4) {
+		alert (" Vamos que se puede. ");
+	}
 }//FIN DE LA FUNCIÓN
