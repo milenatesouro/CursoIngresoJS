47f8632d8adf0f16017b6f9d279f0e766bfec5c6
milenatesouro <mtesour99@gmail.com>
2018-07-24 10:31:02 -0300

    no terminado

--- 5-InstruccionWhile/jsIteraciones (8).js
+++ 5-InstruccionWhile/jsIteraciones (8).js
@@ -4,8 +4,16 @@ function mostrar()
 	var contador=0;
 	var positivo=0;
 	var negativo=1;
-	
+	var numero;
 	var respuesta='si';
+
+	while (respuesta == "si") {
+		contador++;
+		numero = prompt ("Ingrese la cantidad de números deseados.");
+		numero = parseInt (numeros);
+		positivo = 
+		respuesta = Prompt("Ingrese si para continuar ingresando números.")
+	}
 
 
 document.getElementById('suma').value=positivo;
