17438ed5b6a447f54bf75ee4ea79711ff66be59a
milenatesouro <mtesour99@gmail.com>
2018-07-24 10:01:37 -0300

    hecho

--- 5-InstruccionWhile/jsIteraciones (6).js
+++ 5-InstruccionWhile/jsIteraciones (6).js
@@ -4,10 +4,14 @@ function mostrar()
 	var contador=0;
 	var acumulador=0;
 	var numero;
+	var suma=0;
 
 	while (contador<5)	{
+		contador++;
 		numero = prompt ("Ingrese 5 números.");
-		numero = document.getElementById("")
+		numero = parseInt (numero);
+		acumulador = acumulador + numero ;
+		promedio = suma / contador ;
 	}
 	
 
