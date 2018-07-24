c4e0097b38a837f5e1d291d0c26aca528a62f0c6
milenatesouro <mtesour99@gmail.com>
2018-07-24 10:26:13 -0300

    VERIFICAR..

--- 5-InstruccionWhile/jsIteraciones (7).js
+++ 5-InstruccionWhile/jsIteraciones (7).js
@@ -3,7 +3,18 @@ function mostrar()
 
 	var contador=0;
 	var acumulador=0;
+	var suma=0;
 	var respuesta='si';
+	var numero;
+
+	while (respuesta=="si") {
+		contador++;
+		numero = prompt ("Ingrese la cantidad de números deseados.");
+		numero = parseInt (numero);
+		acumulador = suma + numero ;
+		promedio = suma / contador ;
+		respuesta = prompt ("Ingrese si para continuar.");
+	}
 
 
 document.getElementById('suma').value=acumulador;
