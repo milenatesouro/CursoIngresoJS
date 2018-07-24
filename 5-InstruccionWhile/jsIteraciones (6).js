ab69828cec00f1ad51af7b10fa3693ad0d0e6fc7
milenatesouro <mtesour99@gmail.com>
2018-07-24 09:46:32 -0300

    no termina2

--- 5-InstruccionWhile/jsIteraciones (6).js
+++ 5-InstruccionWhile/jsIteraciones (6).js
@@ -3,6 +3,13 @@ function mostrar()
 
 	var contador=0;
 	var acumulador=0;
+	var numero;
+
+	while (contador<5)	{
+		numero = prompt ("Ingrese 5 números.");
+		numero = document.getElementById("")
+	}
+	
 
 
 
