4a7585f12965ceee6f3106ec46a2e8ac1a7a7109
milenatesouro <mtesour99@gmail.com>
2018-07-24 09:37:56 -0300

    hecho

--- 5-InstruccionWhile/jsIteraciones (5).js
+++ 5-InstruccionWhile/jsIteraciones (5).js
@@ -3,7 +3,9 @@ function mostrar()
 
 var sexo = prompt("ingrese f ó m .");
 
-
+while (sexo != "f" && sexo != "m") {
+    sexo = prompt ("error");
+}
 
 document.getElementById('Sexo').value=sexo;
 
