a089bd7d52da03324d46b5d918fb9f64b401d177
milenatesouro <mtesour99@gmail.com>
2018-07-26 10:03:31 -0300

    hecho

--- 3-InstruccionSwitch/jsInstruccionSwitch(3).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(3).js
@@ -2,9 +2,20 @@ function mostrar()
 {
 //tomo la edad  
 var mesDelAño = document.getElementById('mes').value;
+//al seleccionar un mes informar. si es Febrero: " Este mes no tiene más de 29 días." 
+//si NO es Febrero: "Este mes tiene 30 o más días
 
 //alert (mesDelAño);
-	
+switch (mesDelAño)
+{
+    case "Febrero": 
+        alert ("Este mes no tiene mas de 29 dias.");
+    break;
+    default: 
+        alert ("Este mes tiene 30 o mas dias.");
+    break;
+}
+
 	
 
 
