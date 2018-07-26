735891e8367d78468472b5c3d33604b8d22523f1
milenatesouro <mtesour99@gmail.com>
2018-07-26 09:59:55 -0300

    hecho

--- 3-InstruccionSwitch/jsInstruccionSwitch(2).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(2).js
@@ -1,9 +1,33 @@
 function mostrar()
 {
-//tomo la edad  
+//tomo la edad
+//Enunciado:
+//al seleccionar un mes informar. 
+//si estamos en Invierno: "Abrigate que hace frio." 
+//si aún no llego el Invierno: "Falta para el invierno." 
+//si ya paso el Invierno: "Ya pasamos el frio, ahora calor!!!." 
+//ACLARAcIÓN: tomamos a Julio y Agosto como los meses de Invierno.  
+
 var mesDelAño = document.getElementById('mes').value;
 
-alert (mesDelAño);
+switch (mesDelAño)
+{
+    case "Junio":
+        alert ("Abrigate que hace frío.");
+    break; 
+    case "Julio": 
+        alert ("Abrigate que hace frío.");
+    break;
+    case "Agosto":
+        alert ("Abrigate que hace frío.");
+    break;
+    case "Septiembre":
+        alert ("Ya pasamos el frío, ahora calor!!");
+    break;
+    default:
+        alert ("Falta para el invierno.");
+    break;
+}
 
 
 
