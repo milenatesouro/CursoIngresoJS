2b68621236a1b5537ecfc08fbfc46d75113c75f3
milenatesouro <mtesour99@gmail.com>
2018-07-26 10:13:32 -0300

    hecho

--- 3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -4,8 +4,47 @@ function mostrar()
 var mesDelAño = document.getElementById('mes').value;
 
 //alert (mesDelAño);
-	
-	
+//Enunciado: al seleccionar un mes informar. si tiene 28 días. si tiene 30 días. si tiene 31 días.
+
+switch (mesDelAño)
+{
+    case "Enero":
+        alert ("Este mes tiene 31 días.");
+    break;
+    case "Febrero":
+        alert ("Este mes tiene 28 días.");
+    break;
+    case "Marzo":
+        alert ("Este mes tiene 31 días.");
+    break;
+    case "Abril":
+        alert ("Este mes tiene 30 días.");
+    break;
+    case "Mayo":
+        alert ("Este mes tiene 31 días");
+    break;
+    case "Junio":
+        alert ("Este mes tiene 30 días.");
+    break;
+    case "Julio":
+        alert ("Este mes tiene 31 días.");
+    break;
+    case "Agosto":
+        alert ("Este mes tiene 31 días.");
+    break;
+    case "Septiembre":
+        alert ("Este mes tiene 30 días.");
+    break;
+    case "Octubre":
+        alert ("Este mes tiene 31 días.");
+    break;
+    case "Noviembre":
+        alert ("Este mes tiene 30 días.");
+    break;
+    case "Diciembre":
+        alert ("Este mes tiene 31 días.");
+    break;
+}
 
 
 
