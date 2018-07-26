8acfd32efc472fe1be48514f5324aaa2a90a5422
milenatesouro <mtesour99@gmail.com>
2018-07-26 09:42:34 -0300

    hecho

--- 3-InstruccionSwitch/jsInstruccionSwitch(1).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(1).js
@@ -3,7 +3,22 @@ function mostrar()
 //tomo la edad  
 var mesDelAño = document.getElementById('mes').value;
 
-
-
-
+switch(mesDelAño)
+{
+    case "Enero": 
+        alert ("Que comience bien el año!!");
+    break;
+    case "Marzo":
+        alert ("A clases!!");
+    break;
+    case "Julio":
+        alert ("Se vienen las vacaciones!!");
+    break;
+    case "Diciembre":
+        alert ("Felices fiestas!!");
+    break;
+    default: 
+        alert("No se ingreso ningún mes enunciado");
+    break; 
+}
 }//FIN DE LA FUNCIÓN
