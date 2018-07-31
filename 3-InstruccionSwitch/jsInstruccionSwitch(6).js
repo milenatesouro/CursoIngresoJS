55f7736fcfde799f4e7742098464fdee6ba90fb8
milenatesouro <mtesour99@gmail.com>
2018-07-31 10:33:47 -0300

    listo

--- 3-InstruccionSwitch/jsInstruccionSwitch(6).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(6).js
@@ -5,79 +5,35 @@ var laHora = document.getElementById('hora').value;
 switch (laHora)
 {
     case "0":
-        alert ("Es de noche.");
-    break;
     case "1":
-        alert ("Es de noche.");
-    break;
     case "2":
-        alert ("Es de noche.");
-    break;
-    case "3":       
-        alert ("Es de noche."); 
-    break;
+    case "3":
     case "4":
-        alert ("Es de noche.");
-    break;
     case "5":
-        alert ("Es de noche.");
-    break;
     case "6":
-        alert ("Es de noche.");
+    case "20":
+    case "21":
+    case "22":
+    case "23":
+    case "24":
+    alert ("es de noche");
     break;
     case "7":
-        alert ("Es de mañana.");
-    break;
     case "8":
-        alert ("Es de mañana.");
-    break;
     case "9":
-        alert ("Es de mañana.");
-    break;
     case "10":
-        alert ("Es de mañana.");
-    break;
     case "11":
-        alert ("Es de mañana.");
+    alert ("es de mañana");
     break;
     case "12":
-        alert ("Es de tarde.");
-    break;
     case "13":
-        alert ("Es de tarde.");
-    break;
     case "14":
-        alert ("Es de tarde.");
-    break;
     case "15":
-        alert ("Es de tarde.");
-    break;
     case "16":
-        alert ("Es de tarde.");
-    break;
     case "17":
-        alert ("Es de tarde.");
-    break;
     case "18":
-        alert ("Es de tarde.");
-    break;
     case "19":
-        alert ("Es de tarde.");
-    break;
-    case "20":
-        alert ("Es de noche.");
-    break;
-    case "21":
-        alert ("Es de noche.");
-    break;
-    case "22":
-        alert ("Es de noche.");
-    break;
-     case "23":
-        alert ("Es de noche.");
-    break;
-     case "24":
-        alert ("Es de noche.");
+    alert ("es de tarde");
     break;
     default:
         alert ("La hora no existe.");
